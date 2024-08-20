13374cf02e2d032392028f86e409c8c658ffa018
Mohan378513 <mohangowda199605@gmail.com>
2024-08-20 18:57:03 +0530

    Added GitDemo class

---
+++ FreeCRMTest/src/test/java/com/crm/qa/testcases/GitDemo.java
@@ -0,0 +1,13 @@
+package com.crm.qa.testcases;
+
+import org.testng.annotations.Test;
+
+public class GitDemo {
+	
+	@Test
+	public void Demo1() {
+		
+		System.out.println("This is for demo Purpose");
+	}
+
+}
